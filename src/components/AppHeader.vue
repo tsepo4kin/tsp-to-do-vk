86305ff91c4618a3a085d3923e75fb1af64a8699
Kirill <kupuk132@gmail.com>
2021-04-15 04:33:49 +0300

    create DailyTodoList

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    logo
+    <v-spacer></v-spacer>
+    <v-btn text>daily</v-btn>
+    <v-btn text>monthly</v-btn>
+    <v-btn text>Projects</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/DailyTodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <h1 class="text-center display-2 my-5">Todos</h1>
+    <v-row justify="center">
+      <v-col cols="7">
+        <v-text-field
+          @keydown.enter="addTodo"
+          v-model="todo"
+          autocomplete="off"
+          clearable
+          outlined
+          width="25px"
+          label="Input your todo"
+          placeholder="Go to the store"
+          append-outer-icon="mdi-check"
+          @click:append-outer.enter="addTodo"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "DailyTodoList",
+
+  data: () => ({
+    todo: null,
+  }),
+  methods: {
+    addTodo() {
+      if (!this.todo || this.todo.trim() === "") {
+        return;
+      }
+      console.log(1);
+    },
+  },
+};
+</script>
