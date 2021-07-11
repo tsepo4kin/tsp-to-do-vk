126c5cffa2c5c7e2bd153193b93b984c76646dca
Kirill <kupuk132@gmail.com>
2021-07-11 23:23:13 +0300

    update check btn in TodoItem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    todo title btn1 btn2
-    <hr />
+  <div
+    class="d-flex flex-wrap"
+    :class="{ green: isComplete, 'lighten-3': isComplete }"
+  >
+    <span
+      style="width: 390px"
+      :class="{ 'text-decoration-line-through': isComplete }"
+      class="title"
+      >title</span
+    >
+
+    <v-btn
+      @click="isComplete = !isComplete"
+      x-small
+      class="green accent-3 align-self-center"
+      >v</v-btn
+    >
+    <v-btn x-small class="red accent-2 ml-1 align-self-center">x</v-btn>
+    <hr style="width: 100%" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    isComplete: false,
+  }),
+};
 </script>
 
 <style></style>
